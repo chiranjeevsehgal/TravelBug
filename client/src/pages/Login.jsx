69f3f5b0e821f26e437881e7e2ffdf2629d28ae9
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2024-06-22 20:26:16 +0530

    dep

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -38,6 +38,7 @@ const Login = () => {
       const data = await res.json();
       console.log(data);
       if (data?.success) {
+        console.log(data);
         // alert(data.user.username)
         // console.log(data.user);
         dispatch(loginSuccess(data?.user));
