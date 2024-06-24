b18e7d486d83ef8ccb4af39e9ac93b3a716b959c
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2024-06-24 23:23:23 +0530

    Header fix

--- client/src/pages/components/Header.jsx
+++ client/src/pages/components/Header.jsx
@@ -5,14 +5,13 @@ import { FaHome, FaSuitcase, FaUser, FaCaretDown } from "react-icons/fa";
 import defaultProfileImg from "../../assets/images/profile.png";
 import { useNavigate } from "react-router";
 import toast, { Toaster } from 'react-hot-toast';
+import { MdOutlineTravelExplore } from "react-icons/md";
 
 import {
   logOutStart,
   logOutSuccess,
   logOutFailure,
 } from "../../redux/user/userSlice";
-// } from ".../redux/user/userSlice";
-
 
 const Header = () => {
   const navigate = useNavigate();
@@ -26,10 +25,6 @@ const Header = () => {
   const dropdownRef = useRef(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-
-
-  
-
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
@@ -42,8 +37,8 @@ const Header = () => {
     try {
       dispatch(logOutStart());
       const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
-      const res = await fetch(`${API_BASE_URL}/api/auth/logout`,{
-        credentials:"include"
+      const res = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+        credentials: "include"
       });
       const data = await res.json();
       if (data?.success !== true) {
@@ -52,13 +47,10 @@ const Header = () => {
       }
       dispatch(logOutSuccess());
       setIsDropdownOpen(false)
-      
-      
       toast.success(data?.message)
       navigate("/login");
-      
     } catch (error) {
-      toast.error(error)
+      toast.error(error.message)
       console.log(error);
     }
   };
@@ -66,7 +58,6 @@ const Header = () => {
   const handleProfileClick = () => {
     setIsDropdownOpen(false);
     navigate(`/profile/${currentUser.user_role === 1 ? "admin" : "user"}`);
-
   };
 
   useEffect(() => {
@@ -82,125 +73,205 @@ const Header = () => {
     };
   }, []);
 
-  
   return (
-    <header className="bg-white text-gray-900 py-4 px-6 flex justify-between items-center shadow-md">
-      {/* Logo */}
-      <div className="flex items-center">
-        <h1 className="text-2xl font-bold">
-          <Link to="/" className="flex items-center gap-0">
-            <span className="text-[#41A4FF]">Travel</span>
-            <span>Bug</span>
-            <span className="text-[#41A4FF] ml-2">AI</span>
-          </Link>
-        </h1>
-      </div>
+    <>
+      <header className="bg-white text-gray-900 py-4 px-6 flex justify-between items-center shadow-md">
+        {/* Logo */}
+        <div className="flex items-center">
+          <h1 className="text-2xl font-bold">
+            <Link to="/" className="flex items-center gap-0">
+              <span className="text-[#41A4FF]">Travel</span>
+              <span>Bug</span>
+              <span className="text-[#41A4FF] ml-2">AI</span>
+            </Link>
+          </h1>
+        </div>
 
-      {/* Hamburger menu for mobile */}
-      <button
-        onClick={toggleNav}
-        className="block md:hidden text-gray-800 focus:outline-none"
-      >
-        <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+        {/* Hamburger menu for mobile */}
+        <button
+          onClick={toggleNav}
+          className="block md:hidden text-gray-800 focus:outline-none"
         >
-          {isNavOpen ? (
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          ) : (
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          )}
-        </svg>
-      </button>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {isNavOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+        </button>
 
-      {/* Navigation links */}
-      <nav className={`md:flex md:items-center md:space-x-6 ${isNavOpen ? "block" : "hidden"}`}>
-        <ul className="flex flex-col md:flex-row md:items-center gap-6">
-          <li>
-            <Link to="/" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname === "/" ? "text-[#41A4FF]" : ""}`}>
-              <FaHome />
-              <span>Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/packages" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname.includes("/packages") ? "text-[#41A4FF]" : ""}`}>
-              <FaSuitcase />
-              <span>Packages</span>
-            </Link>
-          </li>
-          {currentUser && (
+        {/* Navigation links */}
+        <nav className="hidden md:flex md:items-center md:space-x-6">
+          <ul className="flex flex-col md:flex-row md:items-center gap-6">
             <li>
-              <Link
-                to={`/profile/${currentUser.user_role === 1 ? "dashboard" : "bookings"}`}
-                className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname.includes(currentUser.user_role === 1 ? "/dashboard" : "/bookings") ? "text-[#41A4FF]" : ""
-                  }`}
-              >
-                <FaSuitcase />
-                <span>{currentUser.user_role === 1 ? "Admin Panel" : "Bookings"}</span>
+              <Link to="/" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname === "/" ? "text-[#41A4FF]" : ""}`}>
+                <FaHome />
+                <span>Home</span>
               </Link>
             </li>
-          )}
-          <li>
-            {currentUser ? (
-              <div className="relative" ref={dropdownRef}>
-                <button
-                  onClick={toggleDropdown}
-                  className={`flex items-center gap-2 hover:text-[#41A4FF] ${
-                    location.pathname.includes(currentUser.user_role === 1 ? "/admin" : "/user") ? "text-[#41A4FF]" : ""
-                  }`}
-                >
-                  <img
-                    src={currentUser.avatar || defaultProfileImg}
-                    alt={currentUser.username}
-                    className="w-10 h-10 rounded-full border border-gray-300"
-                  />
-                  <span>{currentUser.username}</span>
-                  <FaCaretDown />
-                </button>
-                {isDropdownOpen && (
-                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
-                    <button
-                      onClick={handleProfileClick}
-                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
-                      Profile
-                    </button>
-                    <button
-                      onClick={handleLogout}
-                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
-                      Logout
-                    </button>
-                  </div>
-                )}
-              </div>
-            ) : (
-              <Link to="/login" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname === "/login" ? "text-[#41A4FF]" : ""}`}>
-                <FaUser />
-                <span>Login</span>
+            <li>
+              <Link to="/packages" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname.includes("/packages") ? "text-[#41A4FF]" : ""}`}>
+              <MdOutlineTravelExplore size={20} />
+                <span>Packages</span>
               </Link>
+            </li>
+            {currentUser && (
+              <li>
+                <Link
+                  to={`/profile/${currentUser.user_role === 1 ? "dashboard" : "bookings"}`}
+                  className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname.includes(currentUser.user_role === 1 ? "/dashboard" : "/bookings") ? "text-[#41A4FF]" : ""
+                    }`}
+                >
+                  <FaSuitcase />
+                  <span>{currentUser.user_role === 1 ? "Admin Panel" : "Bookings"}</span>
+                </Link>
+              </li>
             )}
-          </li>
-        </ul>
-      </nav>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
-    </header>
+            <li>
+              {currentUser ? (
+                <div className="relative" ref={dropdownRef}>
+                  <button
+                    onClick={toggleDropdown}
+                    className={`flex items-center gap-2 hover:text-[#41A4FF] ${
+                      location.pathname.includes(currentUser.user_role === 1 ? "/admin" : "/user") ? "text-[#41A4FF]" : ""
+                    }`}
+                  >
+                    <img
+                      src={currentUser.avatar || defaultProfileImg}
+                      alt={currentUser.username}
+                      className="w-10 h-10 rounded-full border border-gray-300"
+                    />
+                    <span>{currentUser.username}</span>
+                    <FaCaretDown />
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+                      <button
+                        onClick={handleProfileClick}
+                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Profile
+                      </button>
+                      <button
+                        onClick={handleLogout}
+                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Logout
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <Link to="/login" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname === "/login" ? "text-[#41A4FF]" : ""}`}>
+                  <FaUser />
+                  <span>Login</span>
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+        <Toaster position="top-center" reverseOrder={false} />
+      </header>
+
+      {/* Mobile navigation dropdown */}
+      {isNavOpen && (
+        <nav className="md:hidden bg-white shadow-md py-4 px-6 absolute w-full z-10">
+          <ul className="flex flex-col items-center gap-6">
+            <li>
+              <Link
+                to="/"
+                className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname === "/" ? "text-[#41A4FF]" : ""}`}
+                onClick={() => setIsNavOpen(false)}
+              >
+                <FaHome />
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/packages"
+                className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname.includes("/packages") ? "text-[#41A4FF]" : ""}`}
+                onClick={() => setIsNavOpen(false)}
+              >
+                <MdOutlineTravelExplore />
+
+                <span>Packages</span>
+              </Link>
+            </li>
+            {currentUser && (
+              <li>
+                <Link
+                  to={`/profile/${currentUser.user_role === 1 ? "dashboard" : "bookings"}`}
+                  className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname.includes(currentUser.user_role === 1 ? "/dashboard" : "/bookings") ? "text-[#41A4FF]" : ""
+                    }`}
+                  onClick={() => setIsNavOpen(false)}
+                >
+                  <FaSuitcase />
+                  <span>{currentUser.user_role === 1 ? "Admin Panel" : "Bookings"}</span>
+                </Link>
+              </li>
+            )}
+            <li>
+              {currentUser ? (
+                <div className="relative" ref={dropdownRef}>
+                  <button
+                    onClick={toggleDropdown}
+                    className={`flex items-center gap-2 hover:text-[#41A4FF] ${location.pathname.includes(currentUser.user_role === 1 ? "/admin" : "/user") ? "text-[#41A4FF]" : ""
+                      }`}
+                  >
+                    <img
+                      src={currentUser.avatar || defaultProfileImg}
+                      alt={currentUser.username}
+                      className="w-10 h-10 rounded-full border border-gray-300"
+                    />
+                    <span>{currentUser.username}</span>
+                    <FaCaretDown />
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+                      <button
+                        onClick={handleProfileClick}
+                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Profile
+                      </button>
+                      <button
+                        onClick={handleLogout}
+                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Logout
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <Link to="/login" className={`hover:text-[#41A4FF] flex items-center gap-1 ${location.pathname === "/login" ? "text-[#41A4FF]" : ""}`} onClick={() => setIsNavOpen(false)}>
+                  <FaUser />
+                  <span>Login</span>
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
   );
 };
 
